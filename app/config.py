31d1561f0b9f01a9cf7940dd7b8d7a5fe332af2b
hjzui <info.felixklar@gmail.com>
2025-01-03 23:10:07 +0100

    Initial push

--- app/config.py
+++ app/config.py
@@ -6,8 +6,8 @@ DATABASE_CONFIG = {
     'port': 5432
 }
 
-BASE_URL = "https://www.kleinanzeigen.de/s-wohnung-kaufen/braunschweig/wohnungen-wohnung/k0c196l2448"
-PAGE_URL_TEMPLATE = "https://www.kleinanzeigen.de/s-wohnung-kaufen/braunschweig/seite:{page}/wohnungen-wohnung/k0c196l2448"
+BASE_URL = "https://www.kleinanzeigen.de/s-wohnung-mieten/braunschweig/wohnungen/k0c203l2448"
+PAGE_URL_TEMPLATE = "https://www.kleinanzeigen.de/s-wohnung-mieten/braunschweig/seite:{page}/wohnungen/k0c203l2448"
 
 HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
